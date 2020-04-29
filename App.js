7a8d0358fc65d4b84aaa4e98c13ed087f896a78a
kharioki <antolisah@gmail.com>
2020-04-29 15:09:40 +0300

    added assets, and vector icons setup

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {StyleSheet, View, Text, StatusBar, Image} from 'react-native';
+import {StyleSheet, View, StatusBar, Image} from 'react-native';
+
+import FloatingButton from './components/FloatingButton';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <View style={styles.container}>
-        <Text>Learn More</Text>
+        <Image
+          source={require('./assets/gmap.png')}
+          resizeMode="cover"
+          style={{width: 500, height: 900, opacity: 0.5}}
+        />
+
+        <FloatingButton style={{bottom: 0}} />
       </View>
     </>
   );
@@ -14,7 +22,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    flex: 1,
     alignItems: 'center',
   },
 });
---
+++ components/FloatingButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+export default function FloatingButton() {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
