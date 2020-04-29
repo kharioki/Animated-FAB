35e340951d5441c4276a636a13f4b6dc4241bf0f
kharioki <antolisah@gmail.com>
2020-04-29 16:26:52 +0300

    added animation to icons

--- components/FloatingButton.js
+++ components/FloatingButton.js
@@ -12,25 +12,91 @@ AntDesign.loadFont();
 Entypo.loadFont();
 
 export default function FloatingButton(props) {
+  const animation = new Animated.Value(0);
+
+  const toggleMenu = () => {
+    const toValue = this.open ? 0 : 1;
+
+    Animated.spring(animation, {
+      toValue,
+      friction: 5,
+      useNativeDriver: true,
+    }).start();
+
+    this.open = !this.open;
+  };
+
+  const heartStyle = {
+    transform: [
+      {scale: animation},
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -200],
+        }),
+      },
+    ],
+  };
+
+  const thumbStyle = {
+    transform: [
+      {scale: animation},
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -140],
+        }),
+      },
+    ],
+  };
+
+  const pinStyle = {
+    transform: [
+      {scale: animation},
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -80],
+        }),
+      },
+    ],
+  };
+
+  const rotation = {
+    transform: [
+      {
+        rotate: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: ['0deg', '45deg'],
+        }),
+      },
+    ],
+  };
+
+  const opacity = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0, 0, 1],
+  });
+
   return (
     <View style={[styles.container, props.style]}>
       <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.secondary]}>
+        <Animated.View style={[styles.button, styles.secondary, heartStyle]}>
           <AntDesign name="hearto" size={20} color="#f02a4b" />
         </Animated.View>
       </TouchableWithoutFeedback>
       <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.secondary]}>
+        <Animated.View style={[styles.button, styles.secondary, thumbStyle]}>
           <Entypo name="thumbs-up" size={20} color="#f02a4b" />
         </Animated.View>
       </TouchableWithoutFeedback>
       <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.secondary]}>
+        <Animated.View style={[styles.button, styles.secondary, pinStyle]}>
           <Entypo name="location-pin" size={20} color="#f02a4b" />
         </Animated.View>
       </TouchableWithoutFeedback>
-      <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.menu]}>
+      <TouchableWithoutFeedback onPress={toggleMenu}>
+        <Animated.View style={[styles.button, styles.menu, rotation]}>
           <AntDesign name="plus" size={24} color="#fff" />
         </Animated.View>
       </TouchableWithoutFeedback>
@@ -44,7 +110,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   button: {
-    // position: 'absolute',
+    position: 'absolute',
     width: 60,
     height: 60,
     borderRadius: 60 / 2,
